a99ce7e72de8c9426e3d0ffa3dacddc53d468931
Leidy <johisllanos@gmail.com>
2022-12-14 19:58:34 -0500

    Event scroll, keyup para inputs

--- fundamentasl/src/app/app.component.ts
+++ fundamentasl/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'Johana';
@@ -22,5 +22,17 @@ export class AppComponent {
 
   increaseAge () {
     this.person.age +=1;
+  }
+
+  onScroll(event: Event) {
+    //Leer el evento
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
   }
 }

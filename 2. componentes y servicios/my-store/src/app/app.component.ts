20b8bc7d39350523fe26c4093086a81859034cd8
JohanaL <JohanaL>
2023-01-07 21:42:01 -0500

    Inputs y Outputs comunicación entre componentes

--- 2. componentes y servicios/my-store/src/app/app.component.ts
+++ 2. componentes y servicios/my-store/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   imgParent = 'https://www.feelcats.com/wp-content/uploads/2019/03/gatitos.jpg';
+
+  onLoaded(img: string) {
+    console.log('Log Padre', img);
+  }
 }
+  
--- 2. componentes y servicios/my-store/src/app/components/img/img.component.ts
+++ 2. componentes y servicios/my-store/src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ImgComponent implements OnInit {
 
   @Input() img: string = 'valor init';
+  @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/images/default.png';
   
   constructor() { }
@@ -17,5 +18,11 @@ export class ImgComponent implements OnInit {
 
   imgError() {
     this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log("log hijo");
+    this.loaded.emit(this.img);
+    
   }
 }
